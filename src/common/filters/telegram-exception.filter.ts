6d973c36f04e509ca2fc47722a5b697ba37583c0
kaina <masujimk@gmail.com>
2022-09-08 05:01:06 -0400

    send error messages to admin

--- src/common/filters/telegram-exception.filter.ts
+++ src/common/filters/telegram-exception.filter.ts
@@ -3,12 +3,13 @@ import { TelegrafArgumentsHost } from 'nestjs-telegraf';
 
 import type { BotContext } from '@/modules/bot';
 import { ValidationException } from '@/modules/bot/exceptions';
+import { UserService } from '@/modules/user';
 
 import { Logger } from '../logger';
 
 @Catch()
 export class TelegrafExceptionFilter implements ExceptionFilter {
-  constructor(private logger: Logger) {}
+  constructor(private logger: Logger, private userService: UserService) {}
   public async catch(exception: Error, host: ArgumentsHost): Promise<void> {
     const telegrafHost = TelegrafArgumentsHost.create(host);
     const ctx = telegrafHost.getContext<BotContext>();
@@ -16,8 +17,18 @@ export class TelegrafExceptionFilter implements ExceptionFilter {
       await ctx.reply(`${exception.message}`, { parse_mode: 'Markdown' });
     } else {
       await ctx.reply(`*Error*: ${exception.message}`, { parse_mode: 'Markdown' });
+      await this.sendExceptionToAdmin(ctx, exception);
       this.logger.error(exception);
     }
     await ctx.scene.leave();
+  }
+
+  private async sendExceptionToAdmin(ctx: BotContext, exception: Error) {
+    const admin = await this.userService.findSuperAdmin();
+    if (admin) {
+      await ctx.telegram
+        .sendMessage(admin.chatId, `User: ${ctx.user.id}\n${exception.stack}`)
+        .catch(this.logger.error.bind(this));
+    }
   }
 }
